a2a7d0da4df542cda31e2b19a171293e43965a9e
Antonio-2511 <aorovar2011@g.educaand.es>
2026-02-19 12:07:51 +0100

    Estructura y configuracion inicial

---
+++ src/main/java/org/iesalixar/daw2/aovmae/dwese_proyecto_aovmae_webapp_aovmae/DweseProyectoAovmaeWebappAovmaeApplication.java
@@ -0,0 +1,13 @@
+package org.iesalixar.daw2.aovmae.dwese_proyecto_aovmae_webapp_aovmae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DweseProyectoAovmaeWebappAovmaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DweseProyectoAovmaeWebappAovmaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/iesalixar/daw2/aovmae/dwese_proyecto_aovmae_webapp_aovmae/DweseProyectoAovmaeWebappAovmaeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iesalixar.daw2.aovmae.dwese_proyecto_aovmae_webapp_aovmae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DweseProyectoAovmaeWebappAovmaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
